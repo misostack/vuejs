ad00fccdcfc91a92db1864720e514dd45b97e10c
misostack <misostack.com@gmail.com>
2022-10-04 17:50:41 +0700

    add reactivity with ref and reactive

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
 # Vue 3 + TypeScript + Vite
 
+## References
+
+### Add tailwindcss form
+
+- https://github.com/tailwindlabs/tailwindcss-forms
+
+```bash
+npm install -D @tailwindcss/forms
+```
+
+```js
+// tailwind.config.js
+module.exports = {
+  theme: {
+    // ...
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    // ...
+  ],
+};
+```
+
 ## Fundamentals
 
 1. Reactivity
--- src/components/SixJars.vue
+++ src/components/SixJars.vue
@@ -4,11 +4,23 @@
       <div class="flex-none w-64"></div>
       <div class="grow h-14">
         <h1>SixJars</h1>
+        <p>
+          <label>Balance:</label>
+          <input
+            class="form-input px-4 py-3 rounded-full"
+            type="text"
+            v-model.lazy="balance.value"
+            placeholder="0"
+          />
+        </p>
         <h3>Left: {{ state.leftRatio }}</h3>
         <div class="grid grid-cols-6 gap-4 my-8">
           <div v-for="bottle in state.bottles" :key="bottle.name">
             <h3>{{ bottle.name }}</h3>
             <h4>{{ bottle.ratio }}</h4>
+            <h5>
+              {{ parseLocaleNumber((bottle.ratio * balanceValue) / 100) }}
+            </h5>
             <div>
               <button>
                 <span
@@ -34,7 +46,29 @@
 </template>
 
 <script setup lang="ts">
-import { reactive } from "vue";
+import { reactive, ref, watch } from "vue";
+
+// helpers
+function parseLocaleNumber(stringNumber: number, locale: string = "en-EN") {
+  return new Intl.NumberFormat(locale).format(stringNumber);
+}
+
+function reverseLocaleNumber(stringNumber: string, locale: string = "en-EN") {
+  const thousandSeparator = Intl.NumberFormat(locale)
+    .format(11111)
+    .replace(/\p{Number}/gu, "");
+  const decimalSeparator = Intl.NumberFormat(locale)
+    .format(1.1)
+    .replace(/\p{Number}/gu, "");
+
+  return parseFloat(
+    stringNumber
+      .replace(new RegExp("\\" + thousandSeparator, "g"), "")
+      .replace(new RegExp("\\" + decimalSeparator), ".")
+  );
+}
+
+// application
 const initialValues = {
   bottles: [
     { name: "FFA", ratio: 10 },
@@ -48,6 +82,14 @@ const initialValues = {
 };
 // 6 bottles
 const state = reactive(initialValues);
+let balance = reactive({ value: "" });
+let balanceValue = ref(0);
+
+watch(balance, (newBalance, oldBalance) => {
+  const balanceNumber = reverseLocaleNumber(newBalance.value);
+  balanceValue.value = balanceNumber;
+  balance.value = parseLocaleNumber(balanceNumber);
+});
 
 const adjustRatio = (name: string, type: "increase" | "decrease") => {
   for (let i = 0; i < state.bottles.length; i++) {
